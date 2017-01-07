a0ac0c4dc60924f58d7a0ebc3aa0b929ceb21005
Gott50 <xxGott50xx@gmail.com>
2017-01-07 12:03:56 +0100

    add Config option

---
+++ javaScripts/config.js
@@ -0,0 +1,17 @@
+exports.config = [
+    {
+        key: "length",
+        value: "5",
+        then: ["backgrounds", "5 min home 3 min AMRAP", "filter", "foreground elements"],
+    },
+    {
+        key: "length",
+        value: "10",
+        then: ["backgrounds", "10 min home 7 min AMRAP", "filter", "foreground elements"],
+    },
+    {
+        key: "length",
+        value: "30",
+        then: ["backgrounds", "30 min home 20 min AMRAP", "filter", "foreground elements"],
+    },
+];
--- javaScripts/picgen.js
+++ javaScripts/picgen.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 let paste = require("./paste");
+let config = require("./config").config;
+let async = require("async");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    paste.reduce(["backgrounds", "5 min home 3 min AMRAP", "filter", "foreground elements"]);
-    res.render('index', {});
+    console.log(req.query);
+
+    async.each(config, function (item, callback) {
+        if (req.query[item.key] == item.value){
+            paste.reduce(item.then); //TODO add Promise
+            callback("found the key");
+        }
+        else callback;
+    }, function (err) {
+        console.log(err);
+        res.render('index', {})
+    });
 });
 
 module.exports = router;
