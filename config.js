bdf7212227fc266051c1cf9bd2ce6b61bb604d9a
burdaproject <burdaproject@gmail.com>
2017-01-09 06:54:14 +0100

    create home missing lengths

--- config.js
+++ config.js
@@ -16,6 +16,51 @@ module.exports = [
     },
     {
         key: "length",
+        value: "15",
+        then: ["workouts/backgrounds", "workouts/15 min home 10 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "20",
+        then: ["workouts/backgrounds", "workouts/20 min home 13 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "25",
+        then: ["workouts/backgrounds", "workouts/25 min home 15 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "30",
+        then: ["workouts/backgrounds", "workouts/30 min home 20 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "35",
+        then: ["workouts/backgrounds", "workouts/35 min home 25 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "40",
+        then: ["workouts/backgrounds", "workouts/40 min home 30 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "45",
+        then: ["workouts/backgrounds", "workouts/45 min home 30 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "50",
+        then: ["workouts/backgrounds", "workouts/50 min home 30 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
+        value: "55",
+        then: ["workouts/backgrounds", "workouts/55 min home 30 min AMRAP", "workouts/filter", "workouts/foreground elements"],
+    },
+    {
+        key: "length",
         value: "60",
         then: ["workouts/backgrounds", "workouts/60 min home 30 min AMRAP", "workouts/filter", "workouts/foreground elements"],
     },
