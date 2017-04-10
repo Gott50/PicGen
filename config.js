18b13bb6adffb59b403b60a69bb9e46f3bd8b03b
Gott50 <xxGott50xx@gmail.com>
2017-04-10 12:41:25 +0200

    add Pics for WYN home 20-60

--- config.js
+++ config.js
@@ -218,7 +218,13 @@ module.exports = [
     {
         type: "WYN",
         location: "home",
-        duration: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60],
+        duration: [5, 10, 15],
+        src: [],
+    },
+    {
+        type: "WYN",
+        location: "home",
+        duration: [20, 25, 30, 35, 40, 45, 50, 55, 60],
         src: [],
     },
     {
